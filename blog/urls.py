beadf56c4724193494f6e5432a0c3e87635669ee
hayrapetyansami <samihayrapetyan@gmail.com>
2024-11-27 21:11:10 +0400

    user another converters, f.e. slug, re_path, custom converter

--- blog/urls.py
+++ blog/urls.py
@@ -1,8 +1,14 @@
-from django.urls import path, include
+from django.urls import path, include, re_path, register_converter
 from . import views
+
+from . import converters
+
+register_converter(converters.FourDigitYearConverter, "year_from_4_numbers")
 
 urlpatterns = [
 	path("", views.index),
 	path("categories/", views.categories),  # http://127.0.0.1:8000/categories/
 	path("categories/<int:cat_id>", views.categories_by_id),  # http://127.0.0.1:8000/categories/1/
+	path("categories/<slug:cat_slug>", views.categories_by_slug),  # http://127.0.0.1:8000/categories/dd3/
+	path("archive/<year_from_4_numbers:year>/", views.archive),  # http://127.0.0.1:8000/archive/2023/
 ]
--- blog/views.py
+++ blog/views.py
@@ -12,3 +12,11 @@ def categories(request):
 
 def categories_by_id(request, cat_id):
 	return HttpResponse(f"<h1> Categories </h1> <p> ID: {cat_id} </p>")
+
+
+def categories_by_slug(request, cat_slug):
+	return HttpResponse(f"<h1> Categories </h1> <p> SLUG: {cat_slug} </p>")
+
+
+def archive(request, year):
+	return HttpResponse(f"<h1> Archive </h1> <p> Archive: {year} </p>")
