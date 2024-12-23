b5ba7e7f757d2de0d4e97ac94d3d9a88f819bfeb
hayrapetyansami <samihayrapetyan@gmail.com>
2024-12-23 22:13:22 +0400

    crated new table Author, Author connected with Article with Many To One. Crated new custom filrter for Authors, fixed slug bug and multilang (hy, ru, en), and enabled filter horizontal, and explore exclude, fields, readonly, prepopulated_fields

--- blog/admin.py
+++ blog/admin.py
@@ -1,39 +1,72 @@
 from django.contrib import admin, messages
-from .models import Article, Category
+from .models import Article, Category, Author
+
+
+class AuthorFilter(admin.SimpleListFilter):
+    title = "Filter Authors"
+    parameter_name = "check_author"
+
+    def lookups(self, request, model_admin):
+        return [
+            ("with_author", "With Author"),
+            ("without_author", "Without Author"),
+        ]
+
+    def queryset(self, request, queryset):
+        if self.value() == "with_author":
+            return queryset.filter(author__isnull=False)
+        elif self.value() == "without_author":
+            return queryset.filter(author__isnull=True)
+
+
+@admin.register(Author)
+class AuthorAdmin(admin.ModelAdmin):
+    list_display = ["id", "name", "articles_qty"]
+    list_display_links = ["name"]
+    list_per_page = 25
+    ordering = ["-pk"]
+    search_fields = ["name"]
+    search_help_text = "You can search only with author name"
 
 
 @admin.register(Article)
 class ArticleAdmin(admin.ModelAdmin):
-	list_display = ["id", "title", "slug", "is_published", "category", "brief_info"]
-	list_display_links = ["id", "title"]
-	list_editable = ["is_published"]
-	list_per_page = 25
-	ordering = ["-pk"]
-	actions = ["set_published", "set_draft"]
-	search_fields = ["title", "content", "category__name"]
-	search_help_text = "You can search only with title or content"
-	list_filter = ["category__name", "is_published"]
-	
-	# class chgitem (admin.SimpleListFilter) + lookups queryset
-	
-	@admin.display(description="Short Info", ordering="content")
-	def brief_info(self, article: Article):
-		return f"Description {len(article.content)} symbols."
-	
-	@admin.action(description="Դարձնել Փըբլիշդ")
-	def set_published(self, request, queryset):
-		count = queryset.update(is_published=Article.Status.PUBLISHED)
-		self.message_user(request, f"Փըբլիշ է եղել {count} նյութեր", messages.INFO)
-	
-	@admin.action(description="Դարձնել Դրաֆթ")
-	def set_draft(self, request, queryset):
-		count = queryset.update(is_published=Article.Status.DRAFT)
-		self.message_user(request, f"Դրաֆթ է եղել {count} նյութեր", messages.WARNING)
+    # exclude = ["adv"]
+    # fields = ["slug", "title", "content"]
+    # readonly_fields = ["slug"]
+    prepopulated_fields = {"slug": ("title", )}
+    list_display = ["id", "title", "slug",
+                    "is_published", "category", "brief_info", "author"]
+    list_display_links = ["id", "title"]
+    list_editable = ["is_published"]
+    list_per_page = 25
+    ordering = ["-pk"]
+    actions = ["set_published", "set_draft"]
+    search_fields = ["title", "content", "category__name"]
+    search_help_text = "You can search only with title or content"
+    list_filter = [AuthorFilter, "category__name", "is_published"]
+    filter_horizontal = ["tags"]
+
+    @admin.display(description="Short Info", ordering="content")
+    def brief_info(self, article: Article):
+        return f"Description {len(article.content)} symbols."
+
+    @admin.action(description="Դարձնել Փըբլիշդ")
+    def set_published(self, request, queryset):
+        count = queryset.update(is_published=Article.Status.PUBLISHED)
+        self.message_user(request, f"Փըբլիշ է եղել {
+                          count} նյութեր", messages.INFO)
+
+    @admin.action(description="Դարձնել Դրաֆթ")
+    def set_draft(self, request, queryset):
+        count = queryset.update(is_published=Article.Status.DRAFT)
+        self.message_user(request, f"Դրաֆթ է եղել {
+                          count} նյութեր", messages.WARNING)
 
 
 @admin.register(Category)
 class CategoryAdmin(admin.ModelAdmin):
-	list_display = ("id", "name", "slug")
-	list_display_links = ("id", "name")
-	ordering = ["-pk"]
-	list_filter = ["name"]
+    list_display = ("id", "name", "slug")
+    list_display_links = ("id", "name")
+    ordering = ["-pk"]
+    list_filter = ["name"]
--- blog/models.py
+++ blog/models.py
@@ -1,76 +1,95 @@
 from django.db import models
 from django.urls import reverse
+from .utils import slugify
 
 
 class ArticleManager(models.Manager):
-	def get_queryset(self):
-		return super().get_queryset().filter(is_published=Article.Status.PUBLISHED)
+    def get_queryset(self):
+        return super().get_queryset().filter(is_published=Article.Status.PUBLISHED)
 
 
 class Article(models.Model):
-	class Status(models.IntegerChoices):
-		DRAFT = 0, "Draft"
-		PUBLISHED = 1, "Published"
-	
-	title = models.CharField(max_length=255, verbose_name="Վերնագիր")
-	slug = models.SlugField(max_length=255, unique=True, db_index=True, null=True, verbose_name="Ցուցիչ")
-	content = models.TextField(blank=True)
-	time_create = models.DateTimeField(auto_now_add=True)
-	time_update = models.DateTimeField(auto_now=True)
-	is_published = models.BooleanField(choices=tuple(
-		map(lambda x: (bool(x[0]), x[1]), Status.choices)), default=Status.PUBLISHED)
-	category = models.ForeignKey("Category", on_delete=models.CASCADE, related_name="items")
-	tags = models.ManyToManyField("ArticleTags", blank=True, related_name="tags")
-	adv = models.OneToOneField("Adv", on_delete=models.SET_NULL, related_name="adv", null=True, blank=True)
-	
-	objects = models.Manager()
-	published = ArticleManager()
-	
-	def __str__(self):
-		return self.title
-	
-	def get_absolute_url(self):
-		return reverse("article", kwargs={"article_slug": self.slug})
-	
-	class Meta:
-		# verbose_name = "Նյութ"
-		# verbose_name_plural = "Նյութեր"
-		ordering = ["time_create"]
-		indexes = [
-			models.Index(fields=["time_create"])
-		]
+    class Status(models.IntegerChoices):
+        DRAFT = 0, "Draft"
+        PUBLISHED = 1, "Published"
+
+    title = models.CharField(max_length=255, verbose_name="Վերնագիր")
+    slug = models.SlugField(max_length=255, unique=True,
+                            db_index=True, null=True, verbose_name="Ցուցիչ")
+    content = models.TextField(blank=True)
+    time_create = models.DateTimeField(auto_now_add=True)
+    time_update = models.DateTimeField(auto_now=True)
+    is_published = models.BooleanField(choices=tuple(
+        map(lambda x: (bool(x[0]), x[1]), Status.choices)), default=Status.PUBLISHED)
+    category = models.ForeignKey(
+        "Category", on_delete=models.CASCADE, related_name="items")
+    tags = models.ManyToManyField(
+        "ArticleTags", blank=True, related_name="tags")
+    adv = models.OneToOneField(
+        "Adv", on_delete=models.SET_NULL, related_name="adv", null=True, blank=True)
+    author = models.ForeignKey(
+        "Author", on_delete=models.CASCADE, related_name="author")
+
+    objects = models.Manager()
+    published = ArticleManager()
+
+    def __str__(self):
+        return self.title
+
+    def get_absolute_url(self):
+        return reverse("article", kwargs={"article_slug": self.slug})
+
+    def save(self, *args, **kwargs):
+        self.slug = slugify(self.title)
+        super().save(*args, **kwargs)
+
+    class Meta:
+        # verbose_name = "Նյութ"
+        # verbose_name_plural = "Նյութեր"
+        ordering = ["time_create"]
+        indexes = [
+            models.Index(fields=["time_create"])
+        ]
 
 
 class Category(models.Model):
-	name = models.CharField(max_length=255, db_index=True)
-	slug = models.SlugField(max_length=255, unique=True, db_index=True)
-	
-	def __str__(self):
-		return self.name
-	
-	def get_absolute_url(self):
-		return reverse("category", kwargs={"cat_slug": self.slug})
-	
-	class Meta:
-		verbose_name = "Category"
-		verbose_name_plural = "Categories"
+    name = models.CharField(max_length=255, db_index=True)
+    slug = models.SlugField(max_length=255, unique=True, db_index=True)
+
+    def __str__(self):
+        return self.name
+
+    def get_absolute_url(self):
+        return reverse("category", kwargs={"cat_slug": self.slug})
+
+    class Meta:
+        verbose_name = "Category"
+        verbose_name_plural = "Categories"
 
 
 class ArticleTags(models.Model):
-	tag = models.CharField(max_length=255, db_index=True)
-	slug = models.SlugField(max_length=255, unique=True, db_index=True)
-	
-	def __str__(self):
-		return self.tag
-	
-	def get_absolute_url(self):
-		return reverse("tag", kwargs={"tag_slug": self.slug})
+    tag = models.CharField(max_length=255, db_index=True)
+    slug = models.SlugField(max_length=255, unique=True, db_index=True)
+
+    def __str__(self):
+        return self.tag
+
+    def get_absolute_url(self):
+        return reverse("tag", kwargs={"tag_slug": self.slug})
 
 
 class Adv(models.Model):
-	name = models.CharField(max_length=255, db_index=True)
-	budget = models.FloatField(null=True)
-	count_of_sale = models.IntegerField(blank=True, default=0)
-	
-	def __str__(self):
-		return self.name
+    name = models.CharField(max_length=255, db_index=True)
+    budget = models.FloatField(null=True)
+    count_of_sale = models.IntegerField(blank=True, default=0)
+
+    def __str__(self):
+        return self.name
+
+
+class Author(models.Model):
+    name = models.CharField(max_length=255, db_index=True)
+    articles_qty = models.IntegerField(default=0, db_index=True)
+
+    def __str__(self):
+        return self.name
